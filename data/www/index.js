e39fedf3d159220f7abbadfc9e425c2b67ee6f28
nobuyuki furukawa <t_furu@tf-web.jp>
2021-02-17 01:07:48 +0900

    Web画面 修正

--- data/www/index.js
+++ data/www/index.js
@@ -58,11 +58,14 @@ API_TUBE_TO_MP3.stream = (videoId) => {
 }
 
 Vue.use(window['vue-qriously']);
+const VueColor = window.VueColor;
+const Chrome = VueColor.Chrome;
 
 var app = new Vue({
     el: "#app",
     components: {
       'vueSlider': window[ 'vue-slider-component' ],
+      'chrome-picker': Chrome,
     },
     data: {
       title: "ConnectedDoll",
@@ -75,8 +78,22 @@ var app = new Vue({
       volume: {
         min: 0,
         max: 100,
-        value: 100
-      }
+        value: 10
+      },
+      leds: [
+        {
+          "rgba": {r: 255,g: 255,b: 255,a: 1}
+        },
+        {
+          "rgba": {r: 255,g: 255,b: 255,a: 1}
+        },
+        {
+          "rgba": {r: 255,g: 255,b: 255,a: 1}
+        },
+        {
+          "rgba": {r: 255,g: 255,b: 255,a: 1}
+        }
+      ]
     },
     created: function() {
       console.log('created');
@@ -103,66 +120,76 @@ var app = new Vue({
       }
     },
     methods: {
-        clickLedToggle() {
-          // curl -X POST -H "Content-Type: application/json" -d '{"leds": ["255,255,255","255,255,255","255,255,255","255,255,255"]}' http://192.168.86.48/api/led
-          this.isLedOn = !this.isLedOn;
-          const data = {"leds":["0,0,0","0,0,0","0,0,0","0,0,0"]};
-          if (this.isLedOn) {
-            data['leds'][0] = "255,255,255";
-            data['leds'][1] = "255,255,255";
-            data['leds'][2] = "255,255,255";
-            data['leds'][3] = "255,255,255";
-          }
-          API.request("/api/led", data)
-            .then((json)=>{
-              console.log(json);
-            })
-            .catch((e) => {
-              console.error(e);
-            });
-        },
-        clickPlayLocalMp3() {
-          const data = {'path': '/mp3/d3_IcaDhcDM.mp3'};
-          API.request("/api/play/mp3", data)
-            .then((json)=>{
-              console.log(json);
-            })
-            .catch((e) => {
-              console.error(e);
-            });
-        },
-        clickPlayUrlMp3() {
-          const data = {'url': 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'};
-          API.request("/api/play/url/mp3", data)
-            .then((json)=>{
-              console.log(json);
-            })
-            .catch((e) => {
-              console.error(e);
-            });
-        },
-        clickPlayStreamMp3() {
-          // 変換サーバにアクセスして stream URL を取得する
-          const videoId = API_TUBE_TO_MP3.getVideoId(this.youtube.url);
-          API_TUBE_TO_MP3.convert(videoId)
-            .then(result => {
-              // ストリーム URL 生成
-              this.youtube.stream = API_TUBE_TO_MP3.stream(videoId);
+      updateLedcolor() {
+        console.log('this.leds', this.leds);
 
-              // 再生
-              const data = {'url': this.youtube.stream};
-              API.request("/api/play/stream/mp3", data)
-                .then((json)=>{
-                  console.log(json);
-                })
-                .catch((e) => {
-                  console.error(e);
-                });    
-            })
-            .catch(e => {
-              console.error('error', e);
-              alert('エラー ' + e);
-            });
-        }  
+        this.leds[1] = this.leds[0];
+        this.leds[2] = this.leds[0];
+        this.leds[3] = this.leds[0];
+
+        this.isLedOn = false;
+        this.clickLedToggle();
+      },
+      clickLedToggle() {        
+        // curl -X POST -H "Content-Type: application/json" -d '{"leds": ["255,255,255","255,255,255","255,255,255","255,255,255"]}' http://192.168.86.48/api/led
+        this.isLedOn = !this.isLedOn;
+        const data = {"leds":["0,0,0","0,0,0","0,0,0","0,0,0"]};
+        if (this.isLedOn) {
+          data['leds'][0] = this.leds[0]['rgba']['r'] + "," + this.leds[0]['rgba']['g'] + "," + this.leds[0]['rgba']['b'];
+          data['leds'][1] = this.leds[1]['rgba']['r'] + "," + this.leds[1]['rgba']['g'] + "," + this.leds[1]['rgba']['b'];
+          data['leds'][2] = this.leds[2]['rgba']['r'] + "," + this.leds[2]['rgba']['g'] + "," + this.leds[2]['rgba']['b'];
+          data['leds'][3] = this.leds[3]['rgba']['r'] + "," + this.leds[3]['rgba']['g'] + "," + this.leds[3]['rgba']['b'];
+        }
+        API.request("/api/led", data)
+          .then((json)=>{
+            console.log(json);
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+      },
+      clickPlayLocalMp3() {
+        const data = {'path': '/mp3/d3_IcaDhcDM.mp3'};
+        API.request("/api/play/mp3", data)
+          .then((json)=>{
+            console.log(json);
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+      },
+      clickPlayUrlMp3() {
+        const data = {'url': 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'};
+        API.request("/api/play/url/mp3", data)
+          .then((json)=>{
+            console.log(json);
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+      },
+      clickPlayStreamMp3() {
+        // 変換サーバにアクセスして stream URL を取得する
+        const videoId = API_TUBE_TO_MP3.getVideoId(this.youtube.url);
+        API_TUBE_TO_MP3.convert(videoId)
+          .then(result => {
+            // ストリーム URL 生成
+            this.youtube.stream = API_TUBE_TO_MP3.stream(videoId);
+
+            // 再生
+            const data = {'url': this.youtube.stream};
+            API.request("/api/play/stream/mp3", data)
+              .then((json)=>{
+                console.log(json);
+              })
+              .catch((e) => {
+                console.error(e);
+              });    
+          })
+          .catch(e => {
+            console.error('error', e);
+            alert('エラー ' + e);
+          });
+      }  
     }
   });
